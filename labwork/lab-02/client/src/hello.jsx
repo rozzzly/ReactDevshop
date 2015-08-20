7d299aa9ef8c3d918cef325be0779bc86f3f055f
rozzzly <pclienau@gmail.com>
2015-08-20 10:36:02 -0500

    make it pretty

--- labwork/lab-02/client/src/hello.jsx
+++ labwork/lab-02/client/src/hello.jsx
@@ -1,31 +1,29 @@
 var React = require('react/addons');
 
-var Hello = React.createClass(
-	{
+var Hello = React.createClass({
 
-		getInitialState: function() {
-			return {
-				greeting: 'Wazzzup!!'
-			};
-		},
+	getInitialState: function() {
+		return {
+			greeting: 'Wazzzup!!'
+		};
+	},
 
-		getDefaultProps: function() {
-			return {
-				friend: 'My friend'
-			};
-		},
+	getDefaultProps: function() {
+		return {
+			friend: 'My friend'
+		};
+	},
 
-		render: function() {
+	render: function() {
 
-			return (
-				<div className="ui message">
-					<div className="ui huge header">{ this.state.greeting }</div>
-					<div className="ui large header">{ this.props.friend }</div>
-					<p>You are now an expert Browserifier</p>
-				</div>
-			);
-		}
+		return (
+			<div className="ui message">
+				<div className="ui huge header">{ this.state.greeting }</div>
+				<div className="ui large header">{ this.props.friend }</div>
+				<p>I am <strike>batman</strike> browserify</p>
+			</div>
+		);
 	}
-);
+});
 
 module.exports = Hello;
--- labwork/lab-02/client/src/hello.spec.js
+++ labwork/lab-02/client/src/hello.spec.js
@@ -1,32 +1,29 @@
-describe(
-	'Hello World:', function() {
+describe('Hello World:', function() {
+	var Hello,
+	    element,
+	    React,
+	    TestUtils;
 
-		var Hello,
-		    element,
-		    React,
-		    TestUtils;
+	// Require the libraries used for testing.
+	beforeEach(
+		function() {
+			React = require('react/addons');
+			TestUtils = React.addons.TestUtils;
+		}
+	);
 
-		// Require the libraries used for testing.
-		beforeEach(
-			function() {
-				React = require('react/addons');
-				TestUtils = React.addons.TestUtils;
-			}
-		);
+	// Require the components under test.
+	beforeEach(
+		function() {
+			Hello = require('./hello');
+			element = TestUtils.renderIntoDocument(<Hello />);
+		}
+	);
 
-		// Require the components under test.
-		beforeEach(
-			function() {
-				Hello = require('./hello');
-				element = TestUtils.renderIntoDocument(<Hello />);
-			}
-		);
+	it(
+		'should instantiate the Hello World', function() {
+			expect(TestUtils.isCompositeComponent(element)).to.be.true;
+		}
+	);
 
-		it(
-			'should instantiate the Hello World', function() {
-				expect(TestUtils.isCompositeComponent(element)).to.be.true;
-			}
-		);
-
-	}
-);
+});
